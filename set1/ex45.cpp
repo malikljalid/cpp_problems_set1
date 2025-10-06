437090de41bfe4b576a6b7b38169bb2828e7f29b
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-10-06 21:54:06 +0100

    name of a month

---
+++ set1/ex45.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int readMonth(void)
+{
+    int Month;
+
+    std::cout << "Please enter a Month Number : ";
+    std::cin >> Month;
+
+    return (Month - 1); // reduce 1 : to much months names array index
+}
+
+void printMonthName(int Month)
+{
+    std::string Months[12] = { "Junary", "February", "March", "April", "Mai", "June", "July", "Agusut", "September", "October", "November", "December" };
+
+    std::cout << "it's " << Months[Month] << std::endl;
+}
+
+int main(void)
+{
+    printMonthName(readMonth());
+    return (0);
+}
